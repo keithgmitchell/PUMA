f8b7bb0dded1801a53224ffe19628771460e1220
Keith M <keithgmitchell@g.ucla.edu>
2019-05-16 20:37:26 -0700

    one more

--- RunPuma.py
+++ RunPuma.py
@@ -2,4 +2,4 @@ import webbrowser
 from puma import *
 import os
 webbrowser.open('http://127.0.0.1:8000')
-os.system("python manage.py runserver 127.0.0.1:8000")
+os.system("python puma/manage.py runserver 127.0.0.1:8000")
